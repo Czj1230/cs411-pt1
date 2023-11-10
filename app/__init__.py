a95782fab0787087cbf613848a5d04301403a115
Yiming9999 <yimingn2@illinois.edu>
2023-11-09 22:32:32 -0600

    Create person information

--- app/__init__.py
+++ app/__init__.py
@@ -4,9 +4,13 @@ from flask import Flask, render_template, redirect, url_for, request
 app = Flask(__name__)
 
 from app import views
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
-    return render_template('home.html')
+    return render_template('index.html')
+
+@app.route('/second_page')
+def about():
+    return render_template('about.html')
 
 #within app.route add the html page we are doing changes to
 @app.route('/register')
@@ -17,3 +21,10 @@ def register():
 @app.route('/registerV')
 def registerV():
     return render_template('registerV.html')
+
+@app.route('/newpage')
+def newpage():
+    return render_template('newpage.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
