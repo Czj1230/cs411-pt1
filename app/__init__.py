257a7240be343f246c648044b0a336dbc20c5038
Yiming9999 <yimingn2@illinois.edu>
2023-11-09 18:15:36 -0600

    Change web
    666

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template, redirect, url_for, request
+
 
 app = Flask(__name__)
 
 from app import views
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    return render_template('home.html')
+
+#within app.route add the html page we are doing changes to
+@app.route('/register')
+# def is normally how we define a function in python
+def register():
+    return render_template('register.html')
+
+@app.route('/registerV')
+def registerV():
+    return render_template('registerV.html')
