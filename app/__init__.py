338a9c0d0ed2ce6df5962c3dc86c4ad6709377ff
Yiming9999 <yimingn2@illinois.edu>
2023-11-29 14:23:11 -0600

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,12 +9,7 @@ app.config.from_object(config)
 # allow track and modify database
 db.init_app(app)
 
-@game_bp.route('/search')
-def search_games():
-    search_term = request.args.get('query')
-    result = db.session.execute(text(f"SELECT * FROM games WHERE name LIKE '%{search_term}%'"))
-    games = [dict(row) for row in result]
-    return jsonify(games)
+
 
 app.register_blueprint(game_bp)
 
