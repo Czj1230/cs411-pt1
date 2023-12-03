6841079cf5d57f00b6a09c8cdb310b2dc60f0582
Zhuojun Cheng <zhuojun_cheng@outlook.com>
2023-12-02 18:06:24 -0600

    update

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,11 @@ from flask import Flask, render_template, redirect, url_for, request, Blueprint
 from app.backend.games import game_bp
 import config
 from exts import db
+
 from flask import request, jsonify
 from sqlalchemy import text
+
+from app.backend.userHome import userHome_be
 
 app = Flask(__name__)
 
@@ -16,7 +19,7 @@ db.init_app(app)
 
 
 app.register_blueprint(game_bp)
-
+app.register_blueprint(userHome_be)
 
 
 from app import views
