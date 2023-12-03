b16f18a2376259e69ec925128600bea82554b073
camillelii <weiranl4@illinois.edu>
2023-12-02 18:35:13 -0600

    405 error

--- app/backend/login.py
+++ app/backend/login.py
@@ -5,16 +5,33 @@ from sqlalchemy import text
 
 userlog = Blueprint('user', __name__)
 
-@user.route('/signup', methods=['GET', 'POST'])
-def signup():
-    if request.method == 'POST':
-        # Handle the signup form submission
-        return redirect(url_for('main.about'))
-    # If it's a GET request, just render the signup template
-    return render_template('signup.html')
-
 @user.route('/login', methods=['GET', 'POST'])
 def login():
     # Handle the login form submission
-    # ...
-    return render_template('login.html')
+    print("true1")
+    if (request.method == 'POST') :
+        username = request.form['username']
+        password = request.form['password']
+        
+        userexist = db.session.execute(text
+        ("SELECT uid FROM user where uid = " + username + " and password = " + password))
+        # print(userexist)
+
+        if userexist != None:
+            print("true")
+            # Login is successful
+            return render_template('index.html')
+        else:
+            # Login failed
+            flash('Invalid username or password.', 'danger')
+
+    return render_template('userlogin.html')
+
+
+# @user.route('/signup', methods=['GET', 'POST'])
+# def signup():
+#     if request.method == 'POST':
+#         # Handle the signup form submission
+#         return redirect(url_for(''))
+#     # If it's a GET request, just render the signup template
+#     return render_template('signup.html')
