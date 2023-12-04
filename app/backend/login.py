38c9ddea13ddfe64e07f45b4a650e33e41ecac9e
Zhuojun Cheng <zhuojun_cheng@outlook.com>
2023-12-04 00:16:33 -0600

    sign up feature

--- app/backend/login.py
+++ app/backend/login.py
@@ -31,10 +31,25 @@ def login():
     return render_template('userlogin.html', show_alert=True)
 
 
-# @user.route('/signup', methods=['GET', 'POST'])
-# def signup():
-#     if request.method == 'POST':
-#         # Handle the signup form submission
-#         return redirect(url_for(''))
-#     # If it's a GET request, just render the signup template
-#     return render_template('signup.html')
+@userlog.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        # Handle the signup form submission
+        username = request.form['username']
+        password = request.form['password']
+        pre_sql = text("SELECT * FROM user WHERE username = :username")
+        userNameExist = db.session.execute(pre_sql, {'username': username})
+        userNameCnt = userNameExist.fetchall()
+        number_of_rows = len(userNameCnt)
+        if number_of_rows != 0:
+            return render_template('userlogin.html', show_userNameExist=True)
+
+        sql = text("INSERT INTO user(username, password) VALUES (:username, :password)")
+        userAdd = db.session.execute(sql, {'username': username, 'password': password})
+        # user = userAdd.fetchall()
+        # print(user)
+        db.session.commit()
+
+        # return render_template('userlogin.html')
+    # If it's a GET request, just render the signup template
+    return render_template('userlogin.html')
